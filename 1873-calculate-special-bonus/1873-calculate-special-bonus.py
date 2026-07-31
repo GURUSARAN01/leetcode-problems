5ec0aca83da9ad89205a4a709a43b9d4bfcabff0
Gurusaran Sivakumar <78849527+GURUSARAN01@users.noreply.github.com>
2026-07-31 14:21:28 +0200

    Time: 314 ms (57.52%), Space: 68.1 MB (10.96%) - LeetHub

--- 1873-calculate-special-bonus/1873-calculate-special-bonus.py
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.py
@@ -1,7 +1,12 @@
 import pandas as pd
 
 def calculate_special_bonus(employees: pd.DataFrame) -> pd.DataFrame:
-    eligible_employees = employees[~(employees["employee_id"]%2==0) & ~(employees["name"].str.startswith("M"))]
-    eligible_employees=eligible_employees.merge(employees, on="employee_id", how="right").fillna(0)
-    eligible_employees = eligible_employees.rename(columns={"salary_x":"bonus"})
-    return (eligible_employees[["employee_id", "bonus"]].sort_values(by="employee_id",ascending=True))
+    # eligible_employees = employees[~(employees["employee_id"]%2==0) & ~(employees["name"].str.startswith("M"))]
+    # eligible_employees=eligible_employees.merge(employees, on="employee_id", how="right").fillna(0)
+    # eligible_employees = eligible_employees.rename(columns={"salary_x":"bonus"})
+    # return (eligible_employees[["employee_id", "bonus"]].sort_values(by="employee_id",ascending=True))
+
+    employees['bonus']=0
+    employees.loc[(employees['employee_id']%2 !=0) & (~employees['name'].str.startswith("M")), 'bonus'] = employees['salary']
+    result_df = employees[["employee_id", "bonus"]].sort_values(by="employee_id",ascending=True)
+    return result_df
