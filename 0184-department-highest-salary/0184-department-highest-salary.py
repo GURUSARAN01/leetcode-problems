143abcaceae20d125fe0cc8359359bd36435d230
Gurusaran Sivakumar <78849527+GURUSARAN01@users.noreply.github.com>
2026-08-24 17:09:50 +0200

    Time: 404 ms (5%), Space: 68.6 MB (90.72%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.py
+++ 0184-department-highest-salary/0184-department-highest-salary.py
@@ -4,9 +4,9 @@ def department_highest_salary(employee: pd.DataFrame, department: pd.DataFrame) 
     merged = employee.merge(department, left_on = 'departmentId', right_on='id', how='left')
     highest_salary = merged.loc[merged.groupby('departmentId')['salary'].transform('max')==merged['salary']]
     result = highest_salary[['name_x', 'salary', 'name_y']].rename(columns={ 
-        'name_y': 'Department',  # department name column
-        'name_x': 'Employee',    # employee name column
-        'salary': 'Salary'       # salary column
+        'name_y': 'Department', 
+        'name_x': 'Employee',   
+        'salary': 'Salary'     
     })
 
     return result[['Department', 'Employee', 'Salary']]
